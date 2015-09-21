bc2b6db41ca2a5d2d9a5f1252083449ca521a09c
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2015-09-21 23:54:36 +0200

    go fmt

--- hpscantools/do_scan.go
+++ hpscantools/do_scan.go
@@ -1,86 +1,90 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "net/http"
-    "io/ioutil"
-    "encoding/xml"
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 type ScanSettings struct {
-    XMLName         xml.Name    `xml:"http://www.hp.com/schemas/imaging/con/cnx/scan/2008/08/19 ScanSettings"`
-    XResolution     int         `xml:"XResolution"` // 200
-    YResolution     int         `xml:"YResolution"` // 200
-    XStart          int         `xml:"XStart"` // 0
-    YStart          int         `xml:"YStart"` // 0
-    Width           int         `xml:"Width"`  // 2550
-    Height          int         `xml:"Height"` // 3507
-    Format          string      `xml:"Format"` // "Raw", "Jpeg"
-    CompressionQFactor int      `xml:"CompressionQFactor"` // 0, 15
-    ColorSpace      string      `xml:"ColorSpace"` // "Color"
-    BitDepth        int         `xml:"BitDepth"` // 8
-    InputSource     string      `xml:"InputSource"` // "Platen"
-    GrayRendering   string      `xml:"GrayRendering"` // "NTSC"
-    Gamma           int         `xml:"ToneMap>Gamma"` // 1000
-    Brightness      int         `xml:"ToneMap>Brightness"` // 1000
-    Contrast        int         `xml:"ToneMap>Contrast"` // 1000
-    Highlite        int         `xml:"ToneMap>Highlite"` // 179
-    Shadow          int         `xml:"ToneMap>Shadow"` // 25
-    Threshold       int         `xml:"ToneMap>Threshold"` // 0
-    SharpeningLevel int         `xml:"SharpeningLevel"` // 128
-    NoiseRemoval    int         `xml:"NoiseRemoval"` // 0
-    ContentType     string      `xml:"ContentType"` // "Photo", 
+	XMLName            xml.Name `xml:"http://www.hp.com/schemas/imaging/con/cnx/scan/2008/08/19 ScanSettings"`
+	XResolution        int      `xml:"XResolution"`        // 200
+	YResolution        int      `xml:"YResolution"`        // 200
+	XStart             int      `xml:"XStart"`             // 0
+	YStart             int      `xml:"YStart"`             // 0
+	Width              int      `xml:"Width"`              // 2550
+	Height             int      `xml:"Height"`             // 3507
+	Format             string   `xml:"Format"`             // "Raw", "Jpeg"
+	CompressionQFactor int      `xml:"CompressionQFactor"` // 0, 15
+	ColorSpace         string   `xml:"ColorSpace"`         // "Color"
+	BitDepth           int      `xml:"BitDepth"`           // 8
+	InputSource        string   `xml:"InputSource"`        // "Platen"
+	GrayRendering      string   `xml:"GrayRendering"`      // "NTSC"
+	Gamma              int      `xml:"ToneMap>Gamma"`      // 1000
+	Brightness         int      `xml:"ToneMap>Brightness"` // 1000
+	Contrast           int      `xml:"ToneMap>Contrast"`   // 1000
+	Highlite           int      `xml:"ToneMap>Highlite"`   // 179
+	Shadow             int      `xml:"ToneMap>Shadow"`     // 25
+	Threshold          int      `xml:"ToneMap>Threshold"`  // 0
+	SharpeningLevel    int      `xml:"SharpeningLevel"`    // 128
+	NoiseRemoval       int      `xml:"NoiseRemoval"`       // 0
+	ContentType        string   `xml:"ContentType"`        // "Photo",
 }
 
 func DefaultSettings() *ScanSettings {
-    return &ScanSettings{XResolution: 200, YResolution:200, XStart:0, YStart:0, Width: 2550, Height: 3507, Format: "Jpeg", CompressionQFactor: 15, ColorSpace: "Color", BitDepth: 8, InputSource: "Platen", GrayRendering:"NTSC", Gamma: 1000, Brightness: 1000, Contrast: 1000, Highlite: 179,Shadow: 25, Threshold: 0, SharpeningLevel: 128, NoiseRemoval: 0, ContentType: "Photo"}
+	return &ScanSettings{XResolution: 200, YResolution: 200, XStart: 0, YStart: 0, Width: 2550, Height: 3507, Format: "Jpeg", CompressionQFactor: 15, ColorSpace: "Color", BitDepth: 8, InputSource: "Platen", GrayRendering: "NTSC", Gamma: 1000, Brightness: 1000, Contrast: 1000, Highlite: 179, Shadow: 25, Threshold: 0, SharpeningLevel: 128, NoiseRemoval: 0, ContentType: "Photo"}
 }
 
 func JpegScanSettings(xres int, yres int) *ScanSettings {
-    s := DefaultSettings()
-    s.XResolution = xres
-    s.YResolution = yres
-    return s
+	s := DefaultSettings()
+	s.XResolution = xres
+	s.YResolution = yres
+	return s
 }
 
 func RawScanSettings(xres int, yres int) *ScanSettings {
-    s := DefaultSettings()
-    s.XResolution = xres
-    s.YResolution = yres
-    s.Format = "Raw"
-    s.CompressionQFactor = 0
-    return s
+	s := DefaultSettings()
+	s.XResolution = xres
+	s.YResolution = yres
+	s.Format = "Raw"
+	s.CompressionQFactor = 0
+	return s
 }
 
 type CancelScan struct {
-    XMLName         xml.Name    `xml:"http://www.hp.com/schemas/imaging/con/ledm/jobs/2009/04/30 Job"`
-    JobUrl          string                  // The job url from POST-ing SystemSettings
-    JobState        string                  // "Canceled"
+	XMLName  xml.Name `xml:"http://www.hp.com/schemas/imaging/con/ledm/jobs/2009/04/30 Job"`
+	JobUrl   string   // The job url from POST-ing SystemSettings
+	JobState string   // "Canceled"
 }
 
 func main() {
-    s := RawScanSettings(200, 200)
-    xmlString, err := xml.MarshalIndent(s, "", "    ")
+	s := RawScanSettings(200, 200)
+	xmlString, err := xml.MarshalIndent(s, "", "    ")
 
-    if err != nil {
-            fmt.Println(err)
-    }
-    payload := bytes.NewBuffer([]byte ( xml.Header+string(xmlString) ))
+	if err != nil {
+		fmt.Println(err)
+	}
+	payload := bytes.NewBuffer([]byte(xml.Header + string(xmlString)))
 
-    fmt.Printf("%s \n", payload)
+	fmt.Printf("%s \n", payload)
 
-    resp, err := http.Post("http://httpbin.org/post", "test/xml", payload)
-    for k, v := range resp.Header {
-        fmt.Printf("key:%s, value:%s \n", k, v)
-    }
-    location := resp.Header.Get("Location")
-    fmt.Printf("Location: --%s--", location)
+	printer := "http://192.168.0.196:8080"
+	resp, err := http.Post(printer, "test/xml", payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+	location := resp.Header.Get("Location")
+	fmt.Printf("Location: --%s--", location)
+	for k, v := range resp.Header {
+		fmt.Printf("key:%s, value:%s \n", k, v)
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 
-    fmt.Printf("%s \n", body)
+	fmt.Printf("%s \n", body)
 
 }
